4cfa4a90d510b5c51633339d87f5912088d200c0
JimGerman88 <141581580+JimGerman88@users.noreply.github.com>
2024-04-06 19:24:19 +0200

    polebania

--- src/components/Venues/Ceremony.js
+++ src/components/Venues/Ceremony.js
@@ -18,13 +18,9 @@ const Ceremony = () => {
           </div>
         </div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Parkolási lehetőségek: Batthyányi utcáról vagy az Ady Endre útról a templom térre fordulva vagy a kisfaludy utcáról nyiló parkoló. A környéken a parkolás ingyenes 
+                  Megközelítés tömegközlekedéssel:
+                  A közelben áll meg az 50es és a 42es villamos, valamint a  151, 148, 68as busz
         </p>
       </Col>
       <Col lg={6} className="venue-map">
