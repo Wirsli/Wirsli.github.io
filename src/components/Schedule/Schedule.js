bc39d2f39a8c167b3846052c06956a15ec7fcca6
VIOLA\csisz <wirsli@gmail.com>
2024-07-14 14:07:15 +0200

    schedule update

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -3,8 +3,10 @@ import { Container } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChurch,
-  faMugHot,
+  faBreadSlice,
   faUtensils,
+  faCakeCandles,
+  faMusic,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Schedule = () => {
@@ -20,19 +22,42 @@ const Schedule = () => {
             <div className="timeline-content">
               <FontAwesomeIcon icon={faChurch} />
               <h4>Templomi szertartás</h4>
+              <p>
+                A szertartást követően szeretettel várjuk a gratulációkat, majd
+                elkészítjük a csoportfotókat is.
+              </p>
             </div>
           </div>
-          {/* <div className="timeline-row">
+          <div className="timeline-row">
+            <div className="timeline-time">17:00</div>
             <div className="timeline-content">
-              <FontAwesomeIcon icon={faMugHot} />
+              <FontAwesomeIcon icon={faBreadSlice} />
               <h4>Vendégvárás kezdete</h4>
-              <p>Hamarosan bővül...</p>
+              <p>
+                A tangazdaság 17 órától fogadja a kedves vendégeket, de nem
+                szükséges sietni. Aki hoz ajándékot, az kérjük itt adja át.
+              </p>
             </div>
-          </div> */}
+          </div>
+          <div className="timeline-row">
+            <div className="timeline-time">19:00</div>
+            <div className="timeline-content">
+              <FontAwesomeIcon icon={faUtensils} />
+              <h4>Vacsora</h4>
+              <p>Ezután a nyitótánc következik, majd megkezdődik a buli.</p>
+            </div>
+          </div>
+          <div className="timeline-row">
+            <div className="timeline-time">23:00</div>
+            <div className="timeline-content">
+              <FontAwesomeIcon icon={faCakeCandles} />
+              <h4>Torta majd menyecsketánc</h4>
+            </div>
+          </div>
           <div className="timeline-row">
             <div className="timeline-content">
-              {/* <FontAwesomeIcon icon={faUtensils} /> */}
-              <h4>Részletek hamarosan...</h4>
+              <FontAwesomeIcon icon={faMusic} />
+              <h4>Mulatás hajnalig...</h4>
             </div>
           </div>
         </div>
